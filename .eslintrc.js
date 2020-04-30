4e1024f2de7702f3832eb2f826233ffbc796d4b2
David Dooling <david@atomist.com>
2020-04-30 13:49:54 -0500

    Update dependencies, eslint rules, audit
    Update dependencies.  Add eslint rules.  Run `npm audit fix`.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,34 @@
 module.exports = {
     "env": {
         "es6": true,
-        "node": true
+        "node": true,
     },
     "extends": [
         "eslint:recommended",
         "plugin:@typescript-eslint/eslint-recommended",
-        "plugin:@typescript-eslint/recommended"
+        "plugin:@typescript-eslint/recommended",
     ],
     "globals": {
         "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
+        "SharedArrayBuffer": "readonly",
     },
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
         "ecmaVersion": 2018,
-        "sourceType": "module"
+        "project": "tsconfig.json",
+        "sourceType": "module",
     },
     "plugins": [
-        "@typescript-eslint"
+        "@typescript-eslint",
     ],
     "rules": {
-    }
+        "comma-dangle": ["error", "always-multiline"],
+        "comma-spacing": "off",
+        "@typescript-eslint/comma-spacing": ["error"],
+        "eqeqeq": ["error", "always"],
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-floating-promises": ["error"],
+        "semi": "off",
+        "@typescript-eslint/semi": ["error"],
+    },
 };
